3e931ea6fbe5cec231316431d015de40983d54e7
Divide-By-0 <aayushg@mit.edu>
2023-04-16 19:04:11 +0200

    added dockerfile

--- coordinator.py
+++ coordinator.py
@@ -6,17 +6,36 @@ from watchdog.events import FileSystemEventHandler
 import subprocess
 import os
 from dotenv import load_dotenv
-stub = modal.Stub()
 
+def filter_condition(file: str):
+    return ".git/" not in file and "target/" not in file
 
-@stub.webhook(mounts=[
-    modal.Mount.from_local_dir("../", remote_path="/root/")],
+image = modal.Image.from_dockerfile(
+    "./relayer/Dockerfile",
+    context_mount=modal.Mount()
+    .add_local_dir("./relayer", remote_path="/root/relayer", condition=filter_condition)
+    .add_local_dir("./zk-email-verify/build", remote_path="/root/zk-email-verify/build")
+    .add_local_dir("./rapidsnark/build", remote_path="/root/rapidsnark/build")
 )
-def prove_on_email(email_bytes):
-    output_file_path = 'email_1.eml'
+stub = modal.Stub(image=image)
 
-    with open(output_file_path, 'wb') as f:
-        f.write(email_bytes)
+# https://github.com/zkemail/zk-email-verify
+# can cp the local ../relayer
+# but then need to cargo build
+# ../rapidsnark/build
 
-    subprocess.run(["./src/circom_proofgen.sh", "1"])
+@stub.function()
+def test(file_contents: str):
+    return len(file_contents)
+
+# @stub.webhook(mounts=[
+#     modal.Mount.from_local_dir("../", remote_path="/root/")],
+# )
+# def prove_on_email(email_bytes):
+#     output_file_path = 'email_1.eml'
+
+#     with open(output_file_path, 'wb') as f:
+#         f.write(email_bytes)
+
+#     subprocess.run(["./src/circom_proofgen.sh", "1"])
 
