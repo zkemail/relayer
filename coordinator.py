8fa08493fbcbf803c6dd05948d86dffe4c65bad4
Divide-By-0 <aayushg@mit.edu>
2023-04-26 13:15:35 +0200

    coordinator no longer on post webhook, file tracking again

--- coordinator.py
+++ coordinator.py
@@ -7,6 +7,8 @@ import subprocess
 import os
 from dotenv import load_dotenv
 
+LOCAL_OR_MODAL = "modal"
+
 # --------- MODAL CLOUD COORDINATOR ------------
 image = modal.Image.from_dockerhub(
   "aayushg0/zkemail-modal:modal",
@@ -15,8 +17,7 @@ image = modal.Image.from_dockerhub(
 stub = modal.Stub(image=image)
 
 @stub.function(cpu=4)
-@stub.web_endpoint(method="POST")
-def test(file_contents: str, nonce: str):
+def prove_email(file_contents: str, nonce: str):
     # Execute the 'pwd' command and capture its output
     result = subprocess.run(['pwd'], capture_output=True, text=True)
     print("pwd: ", result.stdout.strip())
@@ -25,6 +26,8 @@ def test(file_contents: str, nonce: str):
     file_name = f"wallet_{nonce}.eml"
     with open(file_name, 'w') as file:
         file.write(file_contents)
+
+    print("file_contents: ", file_contents)
 
     # Print the output of the 'proofgen' command
     circom_script_path = "/relayer/src/circom_proofgen.sh"
@@ -44,8 +47,13 @@ class DirectoryChangeHandler(FileSystemEventHandler):
             print(f"New file {event.src_path} has been added.")
             file_name = os.path.basename(event.src_path)
             if (is_eml_file(file_name)):
-                file_name_without_prefix = file_name[file_name.rfind('_') + 1:file_name.rfind('.')]
-                subprocess.run(["./src/circom_proofgen.sh", file_name_without_prefix])
+                with open(file_name, 'r') as file:
+                    email_content = file.read()
+                nonce = file_name[file_name.rfind('_') + 1:file_name.rfind('.')]
+                if LOCAL_OR_MODAL == "local":
+                    subprocess.run(["./src/circom_proofgen.sh", nonce])
+                else:
+                    prove_email(email_content, nonce)
 
 def prove_on_email(path: str):
     event_handler = DirectoryChangeHandler()
