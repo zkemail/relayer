01dd9430431bb314fb0f89972e19ea4cf8dd7f64
Divide-By-0 <aayushg@mit.edu>
2023-10-21 04:56:31 -0400

    latest > nightly

--- common.py
+++ common.py
@@ -16,3 +16,20 @@ image = (
 )
 
 stub = modal.Stub(image=image)
+
+# staging_image = (
+#     modal.Image.from_registry(
+#         "aayushg0/emailwallet:v0",
+#         setup_dockerfile_commands=[
+#             "RUN ls /",
+#             "RUN cp -r /rapidsnark /root/rapidsnark",
+#             "RUN cp -r /relayer /root/relayer",
+#             "RUN cp -r /zk-email-verify /root/zk-email-verify",
+#         ],
+#         add_python="3.10",
+#         # force_build=True
+#     )
+#     .pip_install_from_requirements("requirements.txt")
+# )
+
+# staging_stub = modal.Stub(image=image)
